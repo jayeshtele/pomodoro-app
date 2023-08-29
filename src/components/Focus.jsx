9515308da6c0f34d146639643be27bb0e94a041c
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-29 23:38:37 +0530

    Update Focus.jsx

--- src/components/Focus.jsx
+++ src/components/Focus.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import PauseCircleIcon from "@mui/icons-material/PauseCircle";
+import ReplayIcon from "@mui/icons-material/Replay";
 
 export default function Focus() {
+  const [playPause, setPlayPause] = useState(false);
+
   return (
     <div>
-      <h1 className="text-white mt-20 text-9xl">25:00</h1>
+      <h1 className="mt-20 text-9xl text-white">25:00</h1>
+
+      <div className="playPause mt-10 flex cursor-pointer justify-center space-x-10 text-white">
+        <div
+          className="playPause_toggle"
+          onClick={() => setPlayPause((toggle) => !toggle)}
+        >
+          {playPause ? (
+            <PauseCircleIcon style={{ fontSize: "5em" }} />
+          ) : (
+            <PlayCircleIcon style={{ fontSize: "5em" }} />
+          )}
+        </div>
+
+        <ReplayIcon style={{ fontSize: "5em" }} />
+      </div>
     </div>
   );
 }

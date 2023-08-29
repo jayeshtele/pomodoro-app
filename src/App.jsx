73ba041fe8621cbaddffcb2a26c076bcfca1b36a
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-29 23:09:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Focus from "./components/focus";
+import ShortBreak from "./components/ShortBreak";
 
 export default function App() {
   return (
-    <div className="app flex h-screen flex-col items-center justify-center bg-[#E7626C]">
-     <h1></h1>
+    <div className="app tr flex h-screen flex-col items-center justify-center bg-[#270000]">
+      <h1 className="heading text-6xl font-bold tracking-[1rem] text-[#F4EDDB] mb-[5rem]">
+        POMODORO APP
+      </h1>
+      <div className="navbar text-white space-x-[2rem]">
+        <NavLink className="text-xl font-bold inline-block" to="/">Focus Time</NavLink>
+        <NavLink className="text-xl font-bold inline-block" to="/short">Short Break</NavLink>
+      </div>
+      <Routes>
+        <Route index element={<Focus />} />
+        <Route path="short" element={<ShortBreak />} />
+      </Routes>
     </div>
   );
 }

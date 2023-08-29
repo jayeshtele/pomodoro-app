a721d103085da15b45044ce039ce34016080cf80
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-29 22:40:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app flex h-screen flex-col items-center justify-center bg-[#E7626C]">
+     
+    </div>
+  );
+}

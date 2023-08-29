957a94236a15fbfecb2ae5ab4490c3158857769f
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-29 23:13:32 +0530

    Update ShortBreak.jsx

--- src/components/ShortBreak.jsx
+++ src/components/ShortBreak.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function ShortBreak() {
   return (
     <div>
-      <h1 className="text-white">Short Break</h1>
+      <h1 className="mt-20 text-9xl text-white">05:00</h1>
     </div>
-  )
+  );
 }

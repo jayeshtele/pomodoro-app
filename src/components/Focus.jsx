88aae010f38805882192d629cbd4871f86f383ad
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-29 23:13:30 +0530

    Update Focus.jsx

--- src/components/Focus.jsx
+++ src/components/Focus.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Focus() {
   return (
     <div>
-      <h1 className="text-white">Focus</h1>
+      <h1 className="text-white mt-20 text-9xl">25:00</h1>
     </div>
   );
 }

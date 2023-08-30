579d0fd8c077c829d1117f2879b6313e2e46ca45
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-30 14:12:07 +0530

    Create Firebase.jsx

---
+++ src/firebase/Firebase.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0or1qGuujcGYh16cHg1Ic9q8GFfSFFlQ",
+  authDomain: "pomodoro-app-57856.firebaseapp.com",
+  projectId: "pomodoro-app-57856",
+  storageBucket: "pomodoro-app-57856.appspot.com",
+  messagingSenderId: "1078260940402",
+  appId: "1:1078260940402:web:b3126fe27104eed7124ceb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

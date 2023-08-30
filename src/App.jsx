a53e025ce165018426e701f426c719f4b7c3971e
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-30 17:03:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,14 @@ import React from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
 import Focus from "./components/focus";
 import ShortBreak from "./components/ShortBreak";
+import Login from "./components/Login";
 
 export default function App() {
   return (
     <div className="app tr flex h-screen flex-col items-center justify-center bg-[#270000]">
-      <form className="absolute inset-0 flex items-center justify-center bg-black/80">
-        <div className="form-pomodoro flex flex-col items-center justify-center rounded-xl bg-white px-16 py-[3rem]">
-          <h1 className="mb-16 text-center text-2xl font-bold">Login Form</h1>
-          <div className="username">
-            <label htmlFor="username" className="inline-block w-[6rem] font-bold">
-              Username
-            </label>
-            <input
-              type="text"
-              className="rounded border border-black focus:outline-none"
-              id="username"
-            />
-          </div>
-          <div className="password">
-            <label htmlFor="username" className="mt-5 inline-block w-[6rem] font-bold">
-              Password
-            </label>
-            <input
-              type="text"
-              className="rounded border border-black focus:outline-none"
-              id="username"
-            />
-          </div>
-          <button className="mt-10 inline-block w-full rounded bg-green-800 py-2 font-bold text-white">
-            Login
-          </button>
-        </div>
-      </form>
+
+      <Login />
+
       <h1 className="heading mb-[5rem] text-6xl font-bold tracking-[1rem] text-[#F4EDDB]">
         POMODORO APP
       </h1>

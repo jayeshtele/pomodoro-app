6aef019193d55adf7bce8ff251b8c91a623f5b79
Jayesh Tele <110726388+jayeshtele@users.noreply.github.com>
2023-08-30 18:14:24 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,55 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
+import { auth } from "../firebase/Firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import GoogleButton from "react-google-button";
+import { GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
 
-export default function Login() {
-  const [hide, setHide] = useState(false);
+export default function Logout() {
+  const [user] = useAuthState(auth);
 
-  function hidePage(e) {
-    e.preventDefault();
-    setHide(true);
+  function googleSignIn() {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  }
+
+  function handlelogout() {
+    signOut(auth);
   }
 
   return (
-    <form
-      onSubmit={hidePage}
-      className={`absolute inset-0 ${
-        hide ? "hidden" : "flex"
-      } items-center justify-center bg-black/80`}
-    >
-      <div className="form-pomodoro flex flex-col items-center justify-center rounded-xl bg-white px-16 py-[3rem]">
-        <h1 className="mb-16 text-center text-2xl font-bold">Login Form</h1>
-        <div className="username">
-          <label htmlFor="username" className="inline-block w-[6rem] font-bold">
-            Username
-          </label>
-          <input
-            type="text"
-            className="rounded border border-black focus:outline-none"
-            id="username"
-          />
-        </div>
-        <div className="password">
-          <label
-            htmlFor="username"
-            className="mt-5 inline-block w-[6rem] font-bold"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            className="rounded border border-black focus:outline-none"
-            id="username"
-          />
-        </div>
-        <button className="mt-10 inline-block w-full rounded bg-green-800 py-2 font-bold text-white">
-          Login
+    <>
+      {user ? (
+        <button
+          onClick={handlelogout}
+          className="absolute right-5 top-5 rounded-2xl bg-white px-4 py-2 text-xl font-bold text-[#270000]"
+        >
+          Logout
         </button>
-      </div>
-      <h1
-        onClick={hidePage}
-        className="absolute right-5 top-5 cursor-pointer text-3xl font-bold text-white"
-      >
-        X
-      </h1>
-    </form>
+      ) : (
+        <GoogleButton
+          className="absolute right-5 top-5"
+          onClick={googleSignIn}
+        />
+      )}
+    </>
   );
 }
